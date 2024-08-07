e863f9d77dc6b914d98d4f8fd5e02375a78246f5
Prof. - Xadk3! <151055211+profxadke@users.noreply.github.com>
2024-08-07 08:46:41 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # Create and activate a virtual environment
-pip3 install -U pip uv
-uv venv
+pip3 install -U pip
+curl -LsSf https://astral.sh/uv/install.sh | sh
+cp /tmp/tmp.*/uv .
+chmod +x ./uv
+./uv venv
 source .venv/bin/activate
 
-# Install dependencies
-uv pip3 install -Ur requirements.txt
+# Install dependencies.
+./uv pip install urllib3==1.26.6
+./uv pip install -Ur requirements.txt
 
 # Run the main application
 python3 ./main.py
